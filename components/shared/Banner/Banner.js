9e025fe727ca79285c600cdd3bb355f7e2bcb660
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-15 13:31:24 -0500

    fixed a ' bug

--- components/shared/Banner/Banner.js
+++ components/shared/Banner/Banner.js
@@ -1,11 +1,12 @@
 import { Container } from "./BannerStyles"
 
 export default function Banner () {
+    const hello = `Hi! I'm Nam`
     return (
         <>
         <Container className="m-0 element-center">
             <div>
-                <p className="m-0 p-0 txt-lg fw-5 w-100 text-center"> Hi! I'm Nam </p> 
+                <p className="m-0 p-0 txt-lg fw-5 w-100 text-center"> {hello} </p> 
                 <p className="m-0 p-0 txt-lg fw-5 w-100 text-center"> Welcome to my portfolio </p> 
             </div>
         </Container>
