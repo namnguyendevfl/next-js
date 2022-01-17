e6f4e06f1a9959edb703b6fb9c14efc9ffcd4b06
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-16 23:08:05 -0500

    changed banner width from 100% to 100 vw

--- components/shared/Banner/BannerStyles.js
+++ components/shared/Banner/BannerStyles.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
-    width: 100%;
+    width: 100vw;
     height:80px;
     background: rgb(79 70 229);
     color: white;
